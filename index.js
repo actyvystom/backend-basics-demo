3f3ae6429265407bf5f4234e8f6e1c7a06409796
actyvystom <thomas.goerldt@actyvyst.com>
2023-06-05 11:22:34 +0200

    add comments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// import the server from the file we declared it in
 import { server } from "./server.js";
-
+// set a port variable
 const port = 8000;
+// use the server and its listen method to "listen" for requests at the given port
 server.listen(port, () => {
+  // pass a callback function to e.g. log server running
   console.log(`Server running at http://localhost:${port}`);
 });
